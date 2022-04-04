8b34a36ec4744f04ff3ab96ba613e5f637f880f8
MeanMrMustard42 <stevensuarez426@gmail.com>
2022-04-03 23:37:23 -0700

    Fixed distance formula, re-added try/except blocks because apparently those were needed after all, added comments

--- geometry.py
+++ geometry.py
@@ -1,51 +1,85 @@
+# Steven Suarez
+# CSS 340
+# Program 1
+# 4/4/2022
+
 import math
-from re import X
-from tkinter import Y
 
 class Circle:
 
-    def __init__(self, _x: float = 0, _y: float = 0, _radius: float = 1):
-        self._x = _x
-        self._y = _y
-        self._radius = _radius
+    # Constructor that verifies legitimacy of arguments before they are saved to variables.
+    # If arguments are not legitimate the default values of (0, 0, 1) are used.
 
+    def __init__(self, x: float = 0, y: float = 0, radius: float = 1):
+        try:
+            self._x = float(x)
+        except:
+            self._x = 0
+        try: 
+            self.y = float(y)
+        except:
+            self._y = 0
+        
+        try:
+            self._radius = float(radius)
+        except:
+            self._radius = 1
+
+        if radius >= 0:
+            pass # radius should've already been assigned before here
+        else:
+            self._radius = 1 
+
+
+    # Returns the x value of the origin of this circle.
     def get_x(self):
         return self._x
     
+    # Sets the x-value of the origin of this circle.
     def set_x(self, new_x: float):
-       self._x = new_x
+        try:
+            self._x = float(new_x)
+        except:
+            self._x = 0
 
-
-    def get_x(self):
-        return self._y
-
-    def set_y(self, new_y: float):
-       self._y = new_y
-
+    # Returns the y-value of the origin of this circle.
     def get_y(self):
         return self._y
-        
+
+    # Sets the y-value of the origin of this circle.
+    def set_y(self, new_y: float):
+        try: 
+            self._y = float(new_y)
+        except:
+            self._y = 0
+
+    # Returns the radius of this circle. 
     def get_radius(self):
         return self._radius
 
+
+    # Sets radius of this circle.
     def set_radius(self, new_radius: float):
         if new_radius >= 0:
             self._radius = new_radius
         else:
-            print(new_radius + " is invalid, shutting down")
+            print(str(new_radius) + " is invalid input, shutting down")
             exit()
     
+    # Returns area of this circle.
     def area(self):
         return math.pi*math.pow(self._radius, 2)
 
+    # Returns perimeter of this circle.
     def perimeter(self):
         return 2*math.pi*self._radius
     
+
+    # Bool method that returns whether or not this circle contains a given point, using the Euclidean distance formula
+    # to calculate distance from the origin. Default values are automatically assigned (0,0) upon invalid input.
     def contains_point(self, x, y):
-        
-        if x > (self._x + self._radius) or x < (self._x - self._radius):
+        if math.dist([x, y], [self._x, self._y]) > self._radius:
             return False
-        if y > (self._y + self._radius) or y < (self._y - self._radius):
-            return False
-        return True
-        
+        else:
+            return True
+    
--- test.py
+++ test.py
@@ -1,24 +1,25 @@
 from geometry import Circle 
+
 print("Partial Testing for CSS340 Lab1") 
  
-c1 = Circle(1, 1, 1) 
+c1 = Circle(3, 3, 7) 
 c2 = Circle() 
  
 print("First Circle Perimeter: " + str(c1.perimeter())) 
 print("First Circle Area: " + str(c1.area())) 
  
-c2.set_x(2) 
-c2.set_y(2) 
-c2.set_radius(1) 
-if c2.contains_point(3, 3): 
-    print("(3, 3) is within circle two") 
+c2.set_x(3) 
+c2.set_y(3) 
+c2.set_radius(2) 
+if c2.contains_point(4, 3.7): 
+    print("(4, 3.7) is within circle two") 
 else: 
-    print("(3, 3) is not within circle two") 
+    print("(4, 3.7) is not within circle two") 
  
 print("Moving second Circle") 
-c2.set_x(1 + c2.get_x())
-c2.set_y(1 + c2.get_y())
-if c2.contains_point(3, 3): 
-    print("(3, 3) is within circle two") 
+c2.set_x(3 + c2.get_x()) 
+if c2.contains_point(4, 3.7): 
+    print("(4, 3.7) is within circle two") 
 else: 
-    print("(3, 3) is not within circle two") 
+    print("(4, 3.7) is not within circle two") 
+ 
